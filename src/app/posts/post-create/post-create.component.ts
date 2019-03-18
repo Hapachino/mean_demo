0dd07780c1761f94233dec1b39a54fa796c8c59d
Hapachino <erickwbrownfield@gmail.com>
2019-03-17 17:13:06 -0700

    added post model

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,21 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+import { Post } from '../post.model';
+
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  enteredTitle = '';  
+  enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated: EventEmitter<any> = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
   onAddPost() {
-    const post = { 
+    const post: Post = {
       title: this.enteredTitle,
       content: this.enteredContent,
     };
-    
+
     this.postCreated.emit(post);
   }
 }
